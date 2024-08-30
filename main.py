8d9e483ef3d8351c5cf3b5fa8d25dbbb98f76220
MalikAhmerImran <ahmermalik655@gmail.com>
2024-08-30 17:52:24 +0500

    content pretiffy

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from bs4 import BeautifulSoup
 web=requests.get('https://www.tutorialsfreak.com/')
 
 print(web.status_code)
+
+soup=BeautifulSoup(web.content,'html.parser')
+
+print(soup.prettify())
